5df3773076b2c51265dbe4a7aee2c3fffc75a7f2
cbauschk <bauschkac@gmail.com>
2013-09-09 23:21:28 -0400

    Basic funtionality, returns the substring where the two stings start to differ.

--- src/Diff.java
+++ src/Diff.java
@@ -1,5 +1,41 @@
+import java.lang.String;
+
 public class Diff {
-  public int diff() {
-    return 0;
+  private static final int INDEX_NOT_FOUND = -1;
+  private static final String EMPTY = "";
+
+  public static String diff(String str1, String str2) {
+    if (str1 == null) {
+      return str2;
+    }
+    if (str2 == null) {
+      return str1;
+    }
+
+    int at = indexOfDifference(str1, str2);
+    if (at == INDEX_NOT_FOUND) {
+      return EMPTY;
+    }
+    return str2.substring(at);
+  }
+
+  private static int indexOfDifference(String str1, String str2) {
+    if (str1 == str2) {
+      return INDEX_NOT_FOUND;
+    }
+    if (str1 == null || str2 == null) {
+      return 0;
+    }
+
+    int i;
+    for (i = 0; i < str1.length() && i < str2.length(); i++) {
+      if (str1.charAt(i) != str2.charAt(i)) {
+        break;
+      }
+    }
+    if (i < str1.length() || i < str2.length()) {
+      return i;
+    }
+    return INDEX_NOT_FOUND;
   }
 }
