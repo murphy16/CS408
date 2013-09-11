ebf0780aecb8dd68b953199ea330e4c566855f0a
Kyle Ribordy <kyle@kdribordy.com>
2013-09-11 10:46:47 -0400

    Refactored for flexibility. Added basic tests for find count.

--- src/StatisticsCalculator.java
+++ src/StatisticsCalculator.java
@@ -1,10 +1,16 @@
 public class StatisticsCalculator {
 	
+	private static final char[] WORD_SEPARATORS = {' '}; //what characters do we consider to break up words?
+	private static final char[] BRACKETS_TO_COUNT = {'{','}'};
+	private static final char[] SENTENCE_SEPARATORS = {'.','?','!'}; //what characters do we consider to break up sentences?
+	private static final char[] SPACE_CHARACTERS = {' '};
+
 	public int wordCount(String content) {
+		if(content == null) return 0;
 		int count = 0;
 		boolean inWord = false;
 		for (int i = 0; i < content.length(); i++) {
-			if(content.charAt(i) == ' ') { //what characters do we consider to break up words?
+			if(isCharInSet(content.charAt(i), WORD_SEPARATORS)) {
 				if(inWord) count++;
 				inWord = false;
 			}
@@ -17,9 +23,10 @@ public class StatisticsCalculator {
 	}
 	
 	public int bracketCount(String content) {
+		if(content == null) return 0;
 		int count = 0;
 		for(int i = 0; i < content.length(); i++) {
-			if(content.charAt(i) == '}' || content.charAt(i) == '{') {
+			if(isCharInSet(content.charAt(i), BRACKETS_TO_COUNT)) {
 				count++;
 			}
 		}
@@ -27,10 +34,11 @@ public class StatisticsCalculator {
 	}
 
 	public int sentenceCount(String content) {
+		if(content == null) return 0;
 		int count = 0;
 		boolean inSentence = false;
 		for (int i = 0; i < content.length(); i++) {
-			if(content.charAt(i) == '.' || content.charAt(i) == '!' || content.charAt(i) == '?') { //what characters do we consider to break up words?
+			if(isCharInSet(content.charAt(i), SENTENCE_SEPARATORS)) {
 				if(inSentence) count++;
 				inSentence = false;
 			}
@@ -43,13 +51,18 @@ public class StatisticsCalculator {
 	}
 
 	public int spacesCount(String content) {
+		if(content == null) return 0;
 		int count = 0;
 		for (int i = 0; i < content.length(); i++) {
-			if(content.charAt(i) == ' ') count++;
+			if(isCharInSet(content.charAt(i), SPACE_CHARACTERS)) count++;
 		}
 		return count;
 	}
 	
+	public int findCount(String content, String term) {
+		return 0;
+	}
+
 	private boolean isAlpha(char ch) {
 		if((ch>=65 && ch<=90) || (ch>=97 || ch<=122)) {
 			return true;
@@ -57,6 +70,13 @@ public class StatisticsCalculator {
 		else {
 			return false;
 		}
+	}
+
+	private boolean isCharInSet(char ch, char[] set) {
+		for (char setCh : set) {
+			if (ch == setCh) return true;
+		}
+		return false;
 	}
 
 }
--- src/StatisticsCalculatorTest.java
+++ src/StatisticsCalculatorTest.java
@@ -22,120 +22,162 @@ public class StatisticsCalculatorTest {
 		assertEquals(8, statCalc.wordCount(test1));
 		assertEquals(6, statCalc.wordCount(test2));
 	}
-	
+
 	@Test
 	public void testWordCountEmptyString() {
 		assertEquals(0, statCalc.wordCount(""));
 	}
-	
+
 	@Test
 	public void testWordCountSpecialCharacters() {
 		String test1 = "This is a string with special.characters_which may cause problems";
 		assertEquals(9 , statCalc.wordCount(test1));
 	}
-	
+
 	@Test
 	public void testWordCountMultipleSpaces() {
 		String test = "This is a string with  multiple   spaces separating words        ";
 		assertEquals(9, statCalc.wordCount(test));
 	}
-	
+
 	@Test
 	public void testWordCountLeadingSpace() {
 		String test = "   this string has leading spaces.";
 		assertEquals(5, statCalc.wordCount(test));
 	}
-	
+
 	@Test
 	public void testWordCountOnlySpaces() {
 		String test = "          ";
 		assertEquals(0, statCalc.wordCount(test));
 	}
-	
-	/** BRACKET COUNT TESTS **/
 
+	@Test
+	public void testWordCountNullInput() {
+		assertEquals(0, statCalc.wordCount(null));
+	}
+
+	/** BRACKET COUNT TESTS **/
 	@Test
 	public void testBracketCountEven() {
 		String test = "This is a string with {brackets}. There are {four}.";
 		assertEquals(4, statCalc.bracketCount(test));
 	}
-	
+
 	@Test
 	public void testBracketCountOdd() {
 		String test = "This {string} has an odd number of brackets }";
 		assertEquals(3, statCalc.bracketCount(test));
 	}
-	
+
 	@Test
 	public void testBracketCountEmptyString() {
 		String test = "";
 		assertEquals(0, statCalc.bracketCount(test));
 	}
-	
+
 	@Test
 	public void testBracketCountNoBrackets() {
 		String test = "this string has no brackets! At least no curly ones []()";
 		assertEquals(0, statCalc.bracketCount(test));
 	}
-	
+
+	@Test
+	public void testBracketNullInput() {
+		assertEquals(0, statCalc.bracketCount(null));
+	}
+
+	/** SENTENCE COUNT TESTS **/
 	@Test
 	public void testSentenceCountOneSentence() {
 		String test = "This is a single sentence.";
 		assertEquals(1, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountMultipleSentences() {
 		String test = "This is one sentence. This is another sentence.";
 		assertEquals(2, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountRepeatingPeriods() {
 		String test = "This sentence has multiple periods....Here is another.";
 		assertEquals(2, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountEmptyString() {
 		String test = "";
 		assertEquals(0, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountLeadingPeriod() {
 		String test = ". Why does this string have a leading period. No one knows.";
 		assertEquals(2, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountQuestionMark() {
 		String test = "Is this a question? This is an answer.";
 		assertEquals(2, statCalc.sentenceCount(test));
 	}
-	
+
 	@Test
 	public void testSentenceCountExclamationMark() {
 		String test = "This is exciting! Not really...";
 		assertEquals(2, statCalc.sentenceCount(test));
 	}
-	
+
+	@Test
+	public void testSentenceCountNullInput() {
+		assertEquals(0, statCalc.sentenceCount(null));
+	}
+
+	/** SPACE COUNT TESTS **/
 	@Test
 	public void testSpacesCountEmptyString() {
 		String test = "";
 		assertEquals(0, statCalc.spacesCount(test));
 	}
-	
+
 	@Test
 	public void testSpacesCountNoSpaces() {
 		String test = "thisisastringwithnospaces....butithasotherstuff";
 		assertEquals(0, statCalc.spacesCount(test));
 	}
-	
+
 	@Test
-	public void testSpacesNormalString() {
+	public void testSpacesCountNormalString() {
 		String test = "This string has spaces. It's pretty regular";
 		assertEquals(6, statCalc.spacesCount(test));
 	}
-	
+
+	@Test
+	public void testSpacesCountNullInput() {
+		assertEquals(0, statCalc.spacesCount(null));
+	}
+
+	/**FIND COUNT TESTS **/
+	@Test
+	public void testFindCountNotFound() {
+		String test = "This string doesn't have the search term we are looking for.";
+		String term = "This isn't found";
+		assertEquals(0, statCalc.findCount(test, term));
+	}
+
+	@Test
+	public void testFindCountNoString() {
+		String test = "";
+		String term = "no string? no problem.";
+		assertEquals(0, statCalc.findCount(test, term));
+	}
+
+	@Test
+	public void testFindCountNoTerm() {
+		String test = "this string has words in it.";
+		String term = "";
+		assertEquals(0, statCalc.findCount(test, term));
+	}
 }
